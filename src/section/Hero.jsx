704ace25774ce3117681bc0a32d8edc4b45e893e
Raymond Adu-Danso <raymondadudanso@gmail.com>
2025-07-16 12:23:50 +0000

    mit

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -7,6 +7,9 @@ function Hero() {
            <img src="/images/hero-bg.webp" alt="hero-logo" />
            <img src="/images/hero-text.webp" alt="logo" className="title-logo fade-out" />
            <img src="/images/watch-trailer.png" alt="trailer" className="trailer-logo" />
+           <div className='play-img fade-out'>
+            <img src="/images/play.png" alt="play" className='w-7 ml-1' />
+           </div>
         </div>
     </section>
   )
