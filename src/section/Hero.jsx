8422ce7933ce8017b989a1fdc378e9fd751343c1
Raymond Adu-Danso <raymondadudanso@gmail.com>
2025-07-16 12:36:19 +0000

    comit

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -3,14 +3,24 @@ import React from 'react'
 function Hero() {
   return (
     <section className="hero-section">
+
         <div className="size-full mask-wrapper">
            <img src="/images/hero-bg.webp" alt="hero-logo" />
            <img src="/images/hero-text.webp" alt="logo" className="title-logo fade-out" />
            <img src="/images/watch-trailer.png" alt="trailer" className="trailer-logo" />
+
            <div className='play-img fade-out'>
             <img src="/images/play.png" alt="play" className='w-7 ml-1' />
            </div>
         </div>
+
+           <div>
+              <img src="/images/big-hero-text.svg" alt="logo" className='size-full onject-cover mask-logo' />
+           </div>
+
+           <div className='fake-logo-wrapper'>
+            <img src="/images/big-hero-text.svg" className='overlay-logo' />
+           </div>
     </section>
   )
 }
